5e020793f8622bf39bb82a01b7bb4be388830920
SstickJ <80262644+SstickJ@users.noreply.github.com>
2024-07-03 00:02:22 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,3 +2,23 @@
 
 console.log("Java Script loaded")
 
+//Secret Code
+
+let btnStart = document.getElementById("btn1")
+
+let codigoinput = document.getElementById("input1")
+
+function startA(){
+    startB(codigoinput.value)
+}
+
+function startB(inputedCode){
+    if (inputedCode != ''){
+        console.log("Started")
+        console.log(inputedCode)
+    }else{
+        console.log(inputedCode)
+    }
+}
+
+btnStart.addEventListener("click", startA)
